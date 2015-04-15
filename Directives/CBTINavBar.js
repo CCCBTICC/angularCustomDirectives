b5355554127bead910e4fd7c46cadd941c005006
ypling <leoanrd.li.pub@gmail.com>
2015-04-14 18:59:02 -0700

    NavBar

---
+++ Directives/CBTINavBar.js
@@ -0,0 +1,47 @@
+angular.module('CBTINavBar',[]).directive('navBar',function(){
+    var templateStr=""
+    +"<nav class='navbar navbar-inverse navbar-extra'>"
+    +"    <div class='container'>"
+    +"        <!-- Brand and toggle get grouped for better mobile display -->"
+    +"        <div class='navbar-header'>"
+    +"            <button type='button' class='navbar-toggle collapsed' data-toggle='collapse'"
+    +"                    data-target='#navbar-collapse'>"
+    +"                <span class='sr-only'>Toggle navigation<\/span>"
+    +"                <span class='icon-bar'><\/span>"
+    +"                <span class='icon-bar'><\/span>"
+    +"                <span class='icon-bar'><\/span>"
+    +"            <\/button>"
+    +"            <a class='navbar-brand' href='{{title.link}}'>{{title.text}}<\/a>"
+    +"        <\/div>"
+    +"        <!-- Collect the nav links, forms, and other content for toggling -->"
+    +"        <div class='collapse navbar-collapse' id='navbar-collapse'>"
+    +"            <ul class=\"nav navbar-nav\">"
+    +"                <li id=\"{{menu.text}}\" ng-repeat=\"menu in menuItems\" ng-click=\"menuOnClick(menu.text)\">"
+    +"                    <a href=\"{{menu.link}}\">{{menu.text}}<\/a>"
+    +"                <\/li>"
+    +"            <\/ul>"
+    +"            <ul class='nav navbar-nav navbar-right'>"
+    +"                <li ng-repeat=\"button in buttons\">"
+    +"                    <button type='button' class='btn {{button.type}} btn-block navbar-btn' ng-click=\"button.click()\">"
+    +"                        {{button.text}}"
+    +"                    <\/button>"
+    +"                <\/li>"
+    +"            <\/ul>"
+    +"        <\/div>"
+    +"        <!-- \/.navbar-collapse -->"
+    +"    <\/div>"
+    +"    <!-- \/.container-fluid -->"
+    +"<\/nav>"
+    ;
+
+    return{
+        restrict:'EA',
+        link: function (scope, element, attrs) {
+            scope.menuOnClick = function (id) {
+                $('#' + id).tab('show');
+                $("#navbar-collapse").collapse('hide');
+            }
+        },
+        template:templateStr
+    }
+});
